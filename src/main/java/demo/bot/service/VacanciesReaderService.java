0c6991784a22baccfc3edb95c25ad95c9d893205
Yaroslav Panchishyn <yarpan@gmail.com>
2023-12-29 22:52:00 +0200

    add filereader

---
+++ src/main/java/demo/bot/service/VacanciesReaderService.java
@@ -0,0 +1,32 @@
+package demo.bot.service;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import demo.bot.dto.VacancyDto;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class VacanciesReaderService {
+
+    public List<VacancyDto> getVacanciesFromFile(String fileName) {
+        Resource resource = new ClassPathResource(fileName);
+        try (InputStreamReader inputStreamReader = new InputStreamReader(resource.getInputStream(),
+                StandardCharsets.UTF_8)) {
+            CsvToBean<VacancyDto> csvToBean = new CsvToBeanBuilder<VacancyDto>(inputStreamReader)
+                    .withType(VacancyDto.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBean.parse();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
+    }
+
+
+
+}
